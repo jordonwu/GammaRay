e2b76a584b99c56ee9c81447f1c3fd7890f092c6
Filipe Azevedo <filipe.azevedo@kdab.com>
2016-07-06 15:07:38 +0200

    Fix selection update issue in KLinkItemSelectionModel.
    This issue appear when using a model to link a proxy.
    In some case (especially from QSFPM) a pair of
    layoutAboutToBeChanged/layoutChanged is transformed into a pair of
    layoutAboutToBeChanged/layoutChanged (empty model) and
    rowsAboutToBeInserted/rowsInserted.
    The proxy is so taking much longer time to reflect its changes making
    the KLinkItemSelectionModel trying to update the proxy selection in
    the middle of the layoutChanged/rowsInserted, exactly when it's empty
    (involving unmapped selection which is then simply lost).
    
    Task-Id: KDEND-59

--- 3rdparty/kde/klinkitemselectionmodel.h
+++ 3rdparty/kde/klinkitemselectionmodel.h
@@ -112,6 +112,8 @@ private:
     Q_PRIVATE_SLOT(d_func(), void sourceSelectionChanged(const QItemSelection &selected, const QItemSelection &deselected))
     Q_PRIVATE_SLOT(d_func(), void sourceCurrentChanged(const QModelIndex &current))
     Q_PRIVATE_SLOT(d_func(), void slotCurrentChanged(const QModelIndex &current))
+    Q_PRIVATE_SLOT(d_func(), void slotSourceModelLayoutChanged())
+    Q_PRIVATE_SLOT(d_func(), void slotHandleQueue())
 };
 
 #endif
